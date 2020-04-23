ae28f23ba8e1108b6e3c36b4efd3abbd2012ef44
SornWarisorn <56562334+SornWarisorn@users.noreply.github.com>
2020-04-23 19:01:26 +0700

    update

--- start.py
+++ start.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 os.system('sudo mv protobuf /home/vsa01/Desktop/tensorflow1/models/research')
-os.system('mv os_research.py /home/vsa01/Desktop/tensorflow1/models/research')
+os.system('sudo mv os_research.py /home/vsa01/Desktop/tensorflow1/models/research')
 
 os.chdir('/home/vsa01/Desktop/Train_node/training')
 os.system('python3 mv.py')
