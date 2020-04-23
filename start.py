15f112d7dce075ffde0d0f7eab059588597951b2
SornWarisorn <56562334+SornWarisorn@users.noreply.github.com>
2020-04-23 19:14:49 +0700

    update

--- start.py
+++ start.py
@@ -7,6 +7,9 @@ os.system('sudo mv os_research.py /home/vsa01/Desktop/tensorflow1/models/researc
 os.chdir('/home/vsa01/Desktop/Train_node/training')
 os.system('python3 mv.py')
 
+os.chdir('/home/vsa01/Desktop/tensorflow1/models/research')
+os.system('python3 os_research.py')
+
 
 
 
